a5cdd3188c77dba3daae0cf8ee70a27ca4c36f9d
ulricaleo <1064454416@qq.com>
2018-08-16 15:41:53 +0800

    solution

--- src/com/acm1_10/ACM3.java
+++ src/com/acm1_10/ACM3.java
@@ -1,7 +1,26 @@
 package com.acm1_10;
 
+import java.util.*;
+
+/*题目描述：
+* 去重并排序
+* 输入描述：
+* 输入多行，先输入随机整数的个数，再输入相应个数的整数
+* 输出描述：
+* 排序的数字
+* */
+
 public class ACM3 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Set<Integer> set = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(scanner.nextInt());
+        }
+        scanner.close();
+        for (Iterator<Integer> iterator = set.iterator(); iterator.hasNext(); ) {
+            System.out.println(iterator.next());
+        }
     }
 }
